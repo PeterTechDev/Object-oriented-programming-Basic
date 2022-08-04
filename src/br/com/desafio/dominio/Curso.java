5c697fc09578eebcac16b7dfa34fdabb45b0cd7b
petertechdev <peterleite.dev@mail.com>
2022-08-04 08:07:18 -0300

    create main and basic classes and first object stance

---
+++ src/br/com/desafio/dominio/Curso.java
@@ -0,0 +1,40 @@
+package br.com.desafio.dominio;
+
+public class Curso {
+    private String titulo;
+    private String descricao;
+    private int CargaHoraria;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCargaHoraria() {
+        return CargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        CargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", CargaHoraria=" + CargaHoraria +
+                '}';
+    }
+}
---
+++ src/br/com/desafio/dominio/Mentoria.java
@@ -0,0 +1,42 @@
+package br.com.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+    private String titulo;
+    private String descricacao;
+    private LocalDate data;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricacao() {
+        return descricacao;
+    }
+
+    public void setDescricacao(String descricacao) {
+        this.descricacao = descricacao;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + titulo + '\'' +
+                ", descricacao='" + descricacao + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
